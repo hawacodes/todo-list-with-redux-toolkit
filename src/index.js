6998f2aa08c7062d0a5e10c7a33d0f18a8fe17ec
Havva Beyza Özlüoğlu <beyza.ozluoglu@gmail.com>
2021-09-25 22:34:10 +0300

    delete bootstrap lib from index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
