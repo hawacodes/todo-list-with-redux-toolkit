df47f0af16468f1d2a93f3a48c7756039680ff10
Havva Beyza Özlüoğlu <beyza.ozluoglu@gmail.com>
2021-09-25 22:25:38 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddToDo from "./views/contents/AddToDo";
+import ToDoListView from "./views/contents/ToDoListView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container' style={{ textAlign: 'center', marginTop: '10px' }}>
+      <h1>TO DO List</h1>
+      <AddToDo />
+      <ToDoListView />
     </div>
   );
 }
